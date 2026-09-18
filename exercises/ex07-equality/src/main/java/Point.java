b306e253fe093109563155d03a7dfc17c0bacd18
Sebastian Cooper-Giannotta <109829617+Mr25300@users.noreply.github.com>
2026-09-18 14:05:24 -0400

    Implement Point methods for exercise 7

--- exercises/ex07-equality/src/main/java/Point.java
+++ exercises/ex07-equality/src/main/java/Point.java
@@ -46,8 +46,7 @@ public class Point {
    */
   @Override
   public String toString() {
-    // TODO
-    return "";
+    return "(" + this.x + ", " + this.y + ")";
   }
 
   /**
@@ -58,8 +57,12 @@ public class Point {
    */
   @Override
   public boolean equals(Object o) {
-    // TODO: check that o is a Point (use `instanceof`), cast it, and compare
-    //       the x and y fields.
+    if (o instanceof Point) {
+      Point other = (Point) o;
+
+      return this.x == other.x && this.y == other.y;
+    }
+
     return false;
   }
 
@@ -71,7 +74,6 @@ public class Point {
    */
   @Override
   public int hashCode() {
-    // TODO: Objects.hash(x, y) is an easy way to combine the fields.
-    return 0;
+    return Objects.hash(this.x, this.y);
   }
 }
